44a4d5abc19f1dff9d706fe2b8802e604716c525
lexshpin <lexshpin@gmail.com>
2022-12-11 15:34:00 -0500

    Model for Client done

--- src/main/java/com/lexshpin/OnlineStore/model/Client.java
+++ src/main/java/com/lexshpin/OnlineStore/model/Client.java
@@ -1,6 +1,48 @@
 package com.lexshpin.OnlineStore.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Client")
 public class Client {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    private String username;
+    private String password;
+//    private Cart cart;
 
+    public Client() {}
+
+    public Client(String username, String password) {
+        this.username = username;
+        this.password = password;
+//        this.cart = new Cart();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
